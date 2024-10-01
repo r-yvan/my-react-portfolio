5e71a0f7da270fa2b30edebae0a81f75f6435d74
r-yvan <yvankiliye.rubuto@gmail.com>
2024-10-01 17:18:26 +0200

    Commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,46 +1,17 @@
 import React from "react";
 import background from "../assets/background.png";
 import profile from "../assets/me.png";
+const styles = {
+  backgroundImage: `url(${background})`,
+  backgroundSize: "cover",
+  backgroundRepeat: "no-repeat",
+  height: "100vh",
+};
 
 const About = () => {
   return (
-    <section
-      className="relative bg-gray-950 text-white py-16 px-10 font-body bg-cover bg-center bg-no-repeat"
-      style={{ backgroundImage: `url(${background})` }}
-    >
-      <img src={background} alt="background" />
-      <h2>
-        About <span>Me</span>
-      </h2>
-      <div>
-        <div className="grid grid-cols-7 grid-rows-1">
-          <div className="col-start-3 col-end-5">
-            <h5>
-              <a href="https://git.io/typing-svg">
-                <img
-                  src="https://readme-typing-svg.herokuapp.com?font=Poppins&weight=700&size=60&duration=4000&pause=1000&color=FFFFFF&width=1000&height=100&lines=Hello+There+I'm+Rubuto+Yvan;I'+m+a+FullStack+Developer"
-                  alt="Typing SVG"
-                />
-              </a>
-            </h5>
-            <h1>Full Stack Developer</h1>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-              tincidunt felis nec augue vestibulum, sit amet cursus sapien
-              feugiat. Curabitur sagittis convallis nisl, vel accumsan eros
-              tincidunt in. Donec sodales, metus ac varius lobortis, lacus lorem
-              volutpat urna, sed viverra arcu odio in justo.
-            </p>
-          </div>
-          <div className="col-start-5 col-end-7">
-            <div className="absolute bg-green-400 w-80 h-80 right-80"></div>
-            <div className=""></div>
-            <div className="w-80 h-80 overflow-hidden rounded-2xl">
-              <img className="z-10" src={profile} alt="Rubuto Yvan" />
-            </div>
-          </div>
-        </div>
-      </div>
+    <section className="bg-black text-white font-body" >
+      
     </section>
   );
 };
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -94,10 +94,44 @@ const Header = () => {
           <p className="font-bold">Connections</p>
         </div>
       </div>
-      <div className="flex justify-center pt-10">
-        <button className="px-36 py-3 rounded-full bg-green-600 z-20 font-bold">
+      <div className="flex">
+        <button className="px-36 py-3 rounded-full bg-green-600 z-10 font-bold">
           Connect
         </button>
+      </div>
+      <div className="flex flex-col justify-center pt-10 ">
+        <div className="flex justify-center z-10">
+          <h2 className="text-6xl font-bold">
+            About <span className="text-green-400">Me</span>
+          </h2>
+        </div>
+        <div className="z-10">
+          <div className="col-start-3 col-end-5 z-10">
+            <h5>
+              <a href="https://git.io/typing-svg">
+                <img
+                  src="https://readme-typing-svg.herokuapp.com?font=Poppins&weight=700&size=30&duration=4000&pause=1000&color=FFFFFF&width=1000&height=100&lines=Hello+There+I'm+Rubuto+Yvan;I'+m+a+FullStack+Developer"
+                  alt="Typing SVG"
+                />
+              </a>
+            </h5>
+            <h1>Full Stack Developer</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+              tincidunt felis nec augue vestibulum, sit amet cursus sapien
+              feugiat. Curabitur sagittis convallis nisl, vel accumsan eros
+              tincidunt in. Donec sodales, metus ac varius lobortis, lacus lorem
+              volutpat urna, sed viverra arcu odio in justo.
+            </p>
+          </div>
+          <div className="col-start-5 col-end-7 z-20">
+            <div className="absolute bg-green-400 w-80 h-80 right-80 z-0 !important"></div>
+            <div className=""></div>
+            <div className="w-80 h-80 overflow-hidden rounded-2xl z-20">
+              <img className="" src={profile} alt="Rubuto Yvan" />
+            </div>
+          </div>
+        </div>
       </div>
     </header>
   );
