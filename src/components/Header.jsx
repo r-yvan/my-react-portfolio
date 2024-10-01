998e184f10b5ef6beda5e51cb7597057237fdfc5
r-yvan <yvankiliye.rubuto@gmail.com>
2024-10-01 08:08:30 +0200

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -14,14 +14,18 @@ const Header = () => {
       style={{ minHeight: "100vh" }}
     >
       <nav className="absolute top-0 left-0 right-0 p-5 z-10">
-        {" "}
-        {/* Added z-10 for layering */}
         <ul className="flex justify-center items-center space-x-6 text-lg">
-          <li className="hover:text-green-400 cursor-pointer">Home</li>
-          <li className="hover:text-green-400 cursor-pointer">About Me</li>
-          <li className="hover:text-green-400 cursor-pointer">Education</li>
-          <li className="hover:text-green-400 cursor-pointer">Skills</li>
-          <li className="hover:text-green-400 cursor-pointer">Projects</li>
+          <li className="font-light duration-200 ease-out hover:text-green-400">Home</li>
+          <li className="font-light duration-200 ease-out hover:text-green-400">
+            About Me
+          </li>
+          <li className="font-light duration-200 ease-out hover:text-green-400">
+            Education
+          </li>
+          <li className="font-light duration-200 ease-out hover:text-green-400">Skills</li>
+          <li className="font-light duration-200 ease-out hover:text-green-400">
+            Projects
+          </li>
         </ul>
       </nav>
       <img
@@ -29,26 +33,30 @@ const Header = () => {
         alt="background"
         className="absolute top-0 left-0 w-full h-full object-cover opacity-100 z-0"
       />
-      <div className="mx-auto px-4 py-20 relative z-10 min-h-screen">
+      <div className="mx-auto px-4 py-20 relative z-10">
         <div className="grid grid-cols-5 grid-rows-1 gap-10 mt-24 items-center">
-          <div className="bg-green-500 w-96 h-96 rounded-2xl -z-20 absolute left-80 top-36 rotate-90"></div>
-          <div className="border-4 border-white w-96 h-96 rounded-2xl absolute left-72 top-40 -z-10"></div>
-          <div className="col-end-3 w-96 h-96 rounded-2xl overflow-hidden mb-6 md:mb-0 md:mr-12 shadow-2xl">
+          <div className="bg-green-500 w-80 h-80 rounded-2xl -z-20 absolute left-80 top-36 rotate-90"></div>
+          <div className="border-4 border-white w-80 h-80 rounded-2xl absolute left-72 top-40 -z-10"></div>
+          <div className="col-end-3 w-80 h-80 rounded-2xl overflow-hidden mb-6 md:mb-0 md:mr-12 shadow-2xl">
             <img
               src={profile}
               alt="Rubuto Yvan"
               className="w-full h-full object-cover"
             />
           </div>
-          <div className="col-start-3 col-span-2 pl-16 text-center md:text-left md:ml-12">
+          <div className="col-start-3 col-span-2 text-center md:text-left md:ml-12">
             <h5 className="text-2xl md:text-4xl font-bold">
-              Hello There I'm{" "}
-              <span className="text-green-400">Rubuto Yvan</span>
+              <a href="https://git.io/typing-svg">
+                <img
+                  src="https://readme-typing-svg.herokuapp.com?font=Poppins&weight=700&size=60&duration=4000&pause=1000&color=FFFFFF&width=1000&height=100&lines=Hello+There+I'm+Rubuto+Yvan;I'+m+a+FullStack+Developer"
+                  alt="Typing SVG"
+                />
+              </a>
             </h5>
-            <h1 className="w-full py-6 text-6xl text-green-400 font-bold">
+            <h1 className="w-full py-6 text-5xl text-green-400 font-bold">
               Full Stack Developer
             </h1>
-            <p className="text-gray-300 mt-4 text-lg">
+            <p className="text-gray-300 mt-4 text-md font-light">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
               tincidunt felis nec augue vestibulum, sit amet cursus sapien
               feugiat. Curabitur sagittis convallis nisl, vel accumsan eros
@@ -56,14 +64,36 @@ const Header = () => {
               volutpat urna, sed viverra arcu odio in justo.
             </p>
           </div>
+          <div className="flex justify-center items-center flex-wrap gap-8 mt-12">
+            {/* <img src={netflixLogo} alt="Netflix" className="h-8" /> */}
+            {/* <img src={googleLogo} alt="Google" className="h-8" /> */}
+            {/* <img src={paypalLogo} alt="PayPal" className="h-8" /> */}
+            {/* <img src={skypeLogo} alt="Skype" className="h-8" /> */}
+            {/* <img src={amazonLogo} alt="Amazon" className="h-8" /> */}
+          </div>
         </div>
-        <div className="flex justify-center items-center flex-wrap gap-8 mt-12">
-          {/* <img src={netflixLogo} alt="Netflix" className="h-8" /> */}
-          {/* <img src={googleLogo} alt="Google" className="h-8" /> */}
-          {/* <img src={paypalLogo} alt="PayPal" className="h-8" /> */}
-          {/* <img src={skypeLogo} alt="Skype" className="h-8" /> */}
-          {/* <img src={amazonLogo} alt="Amazon" className="h-8" /> */}
+      </div>
+      <div className="grid grid-cols-7 grid-rows-1 gap-8">
+        <div className="flex justify-center items-center flex-col col-start-3 col-end-4 border-2 border-gray-400 px-4 py-4 bg-white bg-opacity-10 backdrop-filter backdrop-blur-3xl rounded-2xl">
+          <h1 className="text-7xl items-center text-green-400">1+</h1>
+          <p className="opacity-70 font-light pt-3">Years</p>
+          <p>Experience</p>
         </div>
+        <div className="flex justify-center flex-col items-center col-start-4 col-end-5 border-2 border-gray-400 px-10 py-10 bg-white bg-opacity-10 backdrop-filter backdrop-blur-3xl rounded-2xl border-1 border-gray-400">
+          <h1 className="text-7xl items-center text-green-400">21+</h1>
+          <p className="opacity-70 font-light pt-3">Github</p>
+          <p className="font-bold">Repositories</p>
+        </div>
+        <div className="flex justify-center flex-col items-center col-start-5 col-end-6 border-2 border-gray-400 px-4 py-4 bg-white bg-opacity-10 backdrop-filter backdrop-blur-3xl rounded-2xl">
+          <h1 className="text-7xl text-green-400">11+</h1>
+          <p className="opacity-70 font-light pt-3">Friendly</p>
+          <p className="font-bold">Connections</p>
+        </div>
+      </div>
+      <div className="flex justify-center pt-16">
+        <button className="px-36 py-3 rounded-full bg-green-600 z-20 font-bold">
+          Connect
+        </button>
       </div>
     </header>
   );
