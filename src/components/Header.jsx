3e75a7d18d0453ae1ffa320e65c98ebe5df4cb65
r-yvan <yvankiliye.rubuto@gmail.com>
2024-10-03 10:31:43 +0200

    Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -256,7 +256,7 @@ const Header = () => {
         </div>
       </div>
       <Skills />
-      <div className="border-b-4 pb-10 pt-10 border-gray-500 border-opacity-50 z-30">
+      <div className="border-b-4 pb-20 pt-10 border-gray-500 border-opacity-50 z-30">
         <div className="flex justify-center z-20">
           <h2 className="text-4xl font-bold mb-20 z-20">
             My <span className="text-green-400">Projects</span>
@@ -282,22 +282,32 @@ const Header = () => {
         </div>
       </div>
       <div>
-        <h2 className="text-4xl font-bold text-center mb-8">
-          <span className="text-green-400">Clients'</span> Testimonies
-        </h2>
-        <div className="flex flex-wrap justify-center gap-12">
+        <div className="flex justify-center pt-10 z-20">
+          <h2 className="text-4xl font-bold mb-20 z-20">
+            <span className="text-green-400">Clients'</span> Testimonies
+          </h2>
+        </div>
+        <div className="flex justify-center gap-20">
           {testimonials.map((testimonial, index) => (
             <div
-              className="bg-gray-800 p-8 rounded shadow-lg w-64 text-center"
+              className="flex flex-row rounded-2xl border-2 border-green-400 border-opacity-0 duration-200 ease-out hover:border-opacity-100 bg-white bg-opacity-10 backdrop-blur-3xl pl-3 pt-3 shadow-lg w-96"
               key={index}
             >
-              <img
-                src={`./assets/${testimonial.image}`}
-                alt={testimonial.name}
-                className="mx-auto rounded-full w-24 h-24 mb-4"
-              />
-              <p className="text-lg font-semibold mb-2">{testimonial.name}</p>
-              <p className="text-gray-400">{testimonial.comment}</p>
+              <div className="grid grid-cols-[15px_15px_15px_15px_15px_15px_15px_15px_15px_15px] grid-rows-[15px_15px_15px_15px_15px_15px_15px_15px_15px_15px]">
+                <div className="w-28 h-28 bg-green-400 rounded-xl row-start-1 col-start-1"></div>
+                <div className="w-28 h-28 border-white border-4 rounded-2xl mt-2"></div>
+                <div className="w-28 h-28 col-start-3 row-start-2 overflow-hidden rounded-2xl z-20">
+                  <img className="" src={profile} alt="Rubuto Yvan" />
+                </div>
+              </div>
+              <div className="ml-5">
+                <p className="text-lg font-semibold text-green-400 mb-3">
+                  {testimonial.name}
+                </p>
+                <p className="text-gray-400">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                </p>
+              </div>
             </div>
           ))}
         </div>
